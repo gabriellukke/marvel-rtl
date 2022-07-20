a4f24a181d1e79fecde8c7c2b3fcd849e310a3cb
gabriellukke <gabriellukke@outlook.com>
2022-07-20 15:20:03 -0300

    refact: API module authentication

--- src/services/API.js
+++ src/services/API.js
@@ -1,18 +1,16 @@
-import md5 from 'md5';
-
-// API Authentication 
-const baseURL = 'http://gateway.marvel.com/v1/public';
-const publicKey = process.env.REACT_APP_PUBLIC_KEY;
-const privateKey = process.env.REACT_APP_PRIVATE_KEY;
-
-const timeStamp = Number(new Date());
-const hash = md5(timeStamp + privateKey + publicKey);
-
-const urlAuth = `ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`;
+import { baseURL, urlAuth } from './authentication';
 
 export const getAllCharacters = async (limit = '20') => {
   const response = await fetch(
     `${baseURL}/characters?${urlAuth}&limit=${limit.toString()}`
+  );
+  const { data } = await response.json();
+  return data; 
+};
+
+export const getCharacterById = async (characterId) => {
+  const response = await fetch(
+    `${baseURL}/characters/${characterId}${urlAuth}`
   );
   const { data } = await response.json();
   return data; 
